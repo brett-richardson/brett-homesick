98f73aeb809dd195b395bec2610aa928d9647bef
Brett Richardson <brett.richardson.nz@gmail.com>
2018-08-31 11:00:52 +0100

    Add setup script

---
+++ .scripts/setup_machine.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -eu
+
+brew install \
+  ack \
+  awscli \
+  cask \
+  elixir \
+  git \
+  git-lfs \
+  go \
+  htop \
+  irssi \
+  node \
+  nvm \
+  openssl \
+  python \
+  rbenv \
+  ruby \
+  ruby-build \
+  telnet \
+  terraform \
+  tmux \
+  tree \
+  vim \
+  wget \
+  yarn \
+  zsh
+
+brew cask install \
+  alfred \
+  atom \
+  dash \
+  disk-inventory-x \
+  docker \
+  firefox \
+  franz \
+  google-chrome \
+  imageoptim \
+  iterm2 \
+  java \
+  keka \
+  keybase \
+  licecap \
+  macdown \
+  macvim \
+  mounty \
+  nocturne \
+  obs \
+  private-internet-access \
+  skype \
+  slack \
+  sourcetree \
+  spotify \
+  tunnelblick \
+  virtualbox \
+  visual-studio \
+  vlc
